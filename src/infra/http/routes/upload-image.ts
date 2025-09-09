bcc3c73328aeb551b6494ac4067b1f82b05125ad
Rafaela <rafaela.fae@gmail.com>
2025-09-09 10:55:40 -0300

    config: upload-image-function

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { uploadImage } from '@/app/functions/upload-image'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
@@ -7,22 +8,28 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     {
       schema: {
         summary: 'Upload an image',
-        consumes: ['multipart/form-data'], //Formato convencional para enviar arquivos
+        consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.string() }),
-          409: z
-            .object({ message: z.string() })
-            .describe('Upload already exists'),
+          400: z.object({ message: z.string() }),
         },
       },
     },
 
     async (request, reply) => {
       const uploadedFile = await request.file({
-        limits: { fileSize: 1024 * 1024 * 2 }, // 2MB
-      }) // Pega o arquivo enviado
+        limits: { fileSize: 1024 * 1024 * 2 },
+      })
 
-      console.log(uploadedFile)
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required' })
+      }
+
+      await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
 
       return reply.status(201).send({ uploadId: 'test' })
     }
