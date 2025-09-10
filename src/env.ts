3c36994e07409de3c43ee05945b50aeaf0120368
Rafaela <rafaela.fae@gmail.com>
2025-09-10 17:43:10 -0300

    config Cloudflare

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,15 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.url().startsWith('postgresql://'),
-});
 
-export const env = envSchema.parse(process.env);
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY_SECRET: z.string(),
+  CLOUDFLARE_BUCKET: z.string(),
+  CLOUDFLARE_PUBLIC_URL: z.url(),
+})
+
+export const env = envSchema.parse(process.env)
