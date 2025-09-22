160fe73d47264aac9f6d10e1863d060807deb6ec
Rafaela <rafaela.fae@gmail.com>
2025-09-22 18:52:14 -0300

    web: config components visual states

--- web/src/components/upload-widget-upload-list.tsx
+++ web/src/components/upload-widget-upload-list.tsx
@@ -1,16 +1,24 @@
 import { UploadWidgetUploadItem } from './upload-widget-upload-item';
 
 export function UploadWidgetUploadList() {
+	const isUploadListEmpty = false 
+
 	return (
 		<div className="px-3 flex flex-col gap-3">
 			<span className="text-xs font-medium">
 				UploadFiles <span className="text-zinc-400">(2)</span>
 			</span>
 
+			{isUploadListEmpty
+			?(
+				<span className='text-xs text-zinc-400'>No Uploads added</span>
+			)
+			:(
 			<div className="flex flex-col gap-2">
 				<UploadWidgetUploadItem />
 				<UploadWidgetUploadItem />
 			</div>
+			)}
 		</div>
 	);
 }
