69360e9cc7e4f4cc7c22ba5a47cc76ebcdc6c1d7
Rafaela <rafaela.fae@gmail.com>
2025-09-22 10:41:35 -0300

    web: config colapsible widget

--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -1,17 +1,33 @@
-import { UploadWidgetDropzone } from "./upload-widget-dropzone";
-import { UploadWidgetHeader } from "./upload-widget-header";
-import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import * as Collapsible from "@radix-ui/react-collapsible"
+import { UploadWidgetDropzone } from "./upload-widget-dropzone"
+import { UploadWidgetHeader } from "./upload-widget-header"
+import { UploadWidgetUploadList } from "./upload-widget-upload-list"
+import { useState } from "react"
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button"
 
 export function UploadWidget() {
-    return (
-        <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl --shadow-shape">
-            <UploadWidgetHeader />
+    const [isWidgetOpen, seIsWidgetOpen] = useState(false)
 
-            <div className="flex flex-col gap-4 py-3">
-                <UploadWidgetDropzone />
-                <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
-                <UploadWidgetUploadList />
-            </div>
-        </div >
+    return (
+        <Collapsible.Root onOpenChange={seIsWidgetOpen}>
+
+            <div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl --shadow-shape">
+                {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+
+                <Collapsible.Content>
+
+                    <UploadWidgetHeader />
+
+                    <div className="flex flex-col gap-4 py-3">
+                        <UploadWidgetDropzone />
+                        <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+                        <UploadWidgetUploadList />
+                    </div>
+
+                </Collapsible.Content>
+
+            </div >
+
+        </Collapsible.Root >
     )
 }
