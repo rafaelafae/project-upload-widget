e4993e970bcfed6ccc4347559b440d8467105b1e
Rafaela <rafaela.fae@gmail.com>
2025-09-23 12:13:00 -0300

    web: config animations

--- web/src/components/upload-widget-upload-item.tsx
+++ web/src/components/upload-widget-upload-item.tsx
@@ -1,11 +1,17 @@
 import { Download, ImageUp, Link2, RefreshCcw, X } from 'lucide-react';
 import { Button } from './ui/button';
 import * as Progress from '@radix-ui/react-progress';
+import { motion } from 'motion/react';
 
 
 export function UploadWidgetUploadItem() {
 	return (
-		<div className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden">
+		<motion.div 
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 0.5 }}
+			className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden"
+		>
 			<div className="flex flex-col gap-1">
 				<span className="text-xs font-medium flex items-center gap-1">
 					<ImageUp className="size-3 text-zinc-300" strokeWidth={1.5} />
@@ -49,6 +55,6 @@ export function UploadWidgetUploadItem() {
 					<span className="sr-only">Cancel upload</span>
 				</Button>
 			</div>
-		</div>
+		</motion.div>
 	);
 }
--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -2,15 +2,44 @@ import * as Collapsible from '@radix-ui/react-collapsible';
 import { UploadWidgetDropzone } from './upload-widget-dropzone';
 import { UploadWidgetHeader } from './upload-widget-header';
 import { UploadWidgetUploadList } from './upload-widget-upload-list';
-import { useState } from 'react';
 import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button';
+import { motion, useCycle} from 'motion/react'
+
 
 export function UploadWidget() {
-	const [isWidgetOpen, seIsWidgetOpen] = useState(false);
+	const isThereAnyPendingUpload = true
+
+	const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
 
 	return (
-		<Collapsible.Root onOpenChange={seIsWidgetOpen}>
-			<div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl --shadow-shape">
+		<Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
+
+			<motion.div 
+			data-progress={isThereAnyPendingUpload}
+			className="bg-zinc-900 overflow-hidden max-w-[360px] rounded-xl 
+			data-[state=open]:shadow-shape
+			border border-transparent animate-border-spin 
+			data-[state=closed]:rounded-3xl 
+			data-[state=closed]:data-[progress=false]:shadow-shape 
+			data-[state=closed]:data-[progress=true]:border-gradient"
+			animate={isWidgetOpen ? 'open' : 'closed'}
+			variants={{
+				closed: {
+					width: 'max-content',
+					height: 44,
+					transition: {
+						type: 'inertia',
+					}
+				},
+				open: {
+					width: 360,
+					height: 'auto',
+					transition: {
+						duration: 0.1,
+					}
+				},
+			}}
+			>
 				{!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
 				<Collapsible.Content>
@@ -22,7 +51,8 @@ export function UploadWidget() {
 						<UploadWidgetUploadList />
 					</div>
 				</Collapsible.Content>
-			</div>
+			</motion.div>
+
 		</Collapsible.Root>
 	);
 }
